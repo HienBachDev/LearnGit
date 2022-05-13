1b218a054b2a8dd6bc27ced68e5f1834c132addc
DINHKHANH2170 <102986242+DINHKHANH2170@users.noreply.github.com>
2022-05-13 23:38:15 +0700

    Update HELLO.cpp
    add hello world

--- HELLO.cpp
+++ HELLO.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int a;
 int main()
 {
+	cout << i <<"hello world  " << n/i <<endl; 
 	int n ;
 	cin >> n;
 	for (int i = int(sqrt(n)); i > 0 ;i--)
@@ -11,6 +12,7 @@ int main()
 		if (n%i ==0 )
 		{	
 		cout << i <<" " << n/i <<endl; 
+			
 	    break;
 		}
 	}
